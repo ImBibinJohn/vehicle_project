b9246db5682ae6d4ee62ea7e8008093d7e7484a4
Bibin John <codebibinjohn@gmail.com>
2023-05-10 17:57:01 +0530

    form created

--- app/models.py
+++ app/models.py
@@ -8,3 +8,23 @@ class UserDetails(models.Model):
 
     def __str__(self):
         return self.username
+
+class AgentVehicle(models.Model):
+    vehicle_registration = models.CharField(max_length=50)
+    vin_number = models.CharField(max_length=17)
+    vehicle_year = models.PositiveIntegerField()
+    vehicle_make = models.CharField(max_length=50)
+    vehicle_model = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    mileage = models.PositiveIntegerField()
+    number_etched_into_windows = models.CharField(max_length=50)
+    vehicle_name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=150)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.vehicle_registration
